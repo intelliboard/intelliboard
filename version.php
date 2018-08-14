618158b9b7fd53e4d485bcbeb0a09bb297372e99
IntelliBoard <anatoliy.kochnev@sebale.net>
2018-08-14 13:44:07 -0400

    Bug fixes and performance improvements

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  * @website    https://intelliboard.net/
  */
 
-$plugin->version = 2018090400;
+$plugin->version = 2018091400;
 $plugin->requires = 2011120500;
 $plugin->release = '5.1';
 $plugin->maturity = MATURITY_STABLE;
