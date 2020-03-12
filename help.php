cb6a6595ea2d57446bb46cf4ae2fece6c5437ca0
Taras Leshko <taras@intelliboard.net>
2020-03-11 21:30:36 -0400

    Bug fixes and performance improvements

--- help.php
+++ help.php
@@ -30,6 +30,7 @@ require_once($CFG->dirroot .'/local/intelliboard/instructor/lib.php');
 
 require_login();
 
+require_capability('local/intelliboard:view', context_system::instance());
 
 $teacher_roles = get_config('local_intelliboard', 'filter10');
 $learner_roles = get_config('local_intelliboard', 'filter11');
@@ -65,107 +66,130 @@ $PAGE->requires->jquery();
 echo $OUTPUT->header();
 ?>
 
-<?php if(!is_siteadmin()): ?>
-    <div class="alert alert-error alert-block" role="alert"><?php echo get_string('intelliboardaccess', 'local_intelliboard'); ?></div>
-<?php else: ?>
-
-    <div class="intelliboard-support">
-        <div class="intelliboard-support-menu">
-            <?php if (empty($intelliboard->token)): ?>
-                <a href="<?php echo $connectlink->out(); ?>">
-                    <?php echo get_string('support_trial', 'local_intelliboard'); ?>
-                </a>
-            <?php else: ?>
-                <a href="<?php echo $CFG->wwwroot ?>/local/intelliboard/<?php echo $event ?>/index.php?action=<?php echo ($event)?'':'dashboard' ?>">
-                    <?php echo get_string('dashboard', 'local_intelliboard'); ?>
-                </a>
-            <?php endif; ?>
-        </div>
-        <div class="intelliboard-support-logo">
-            <a target="_blank" href="https://intelliboard.net/">
-                <img src="<?php echo $CFG->wwwroot ?>/local/intelliboard/assets/img/logo@3x.png">
+<div class="intelliboard-support">
+    <div class="intelliboard-support-menu">
+        <?php if (empty($intelliboard->token) && is_siteadmin()): ?>
+            <a href="<?php echo $connectlink->out(); ?>">
+                <?php echo get_string('support_trial', 'local_intelliboard'); ?>
             </a>
-        </div>
-
-        <div class="clear"></div>
-
-        <?php if (empty($intelliboard->token)): ?>
-            <div class="intelliboard-support-text">
-                <p><?php echo get_string('support_text3', 'local_intelliboard', ["connect_link" => $connectlink->out()]); ?></p>
-            </div>
-            <div class="intelliboard-support-links">
-                <a href="<?php echo $CFG->wwwroot ?>/local/intelliboard/<?php echo $event ?>/index.php?action=<?php echo ($event)?'':'dashboard' ?>"
-                   class="intelliboard-support-btn"
-                >
-                    <?php echo get_string('dashboard', 'local_intelliboard'); ?>
-                </a>
-            </div>
-            <div class="intelliboard-support-links actions">
-                <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.acuityscheduling.com/schedule.php">
-                    <span><?php echo get_string('support_demo', 'local_intelliboard'); ?></span>
-                </a>
-                <a class="intelliboard-support-large-btn" target="_blank"
-                   href="https://support.intelliboard.net/hc/en-us/categories/360000794431-IntelliBoard-for-Moodle-">
-                    <span><?php echo get_string('support_page', 'local_intelliboard'); ?></span>
-                </a>
-                <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.net/events">
-                    <span><?php echo get_string('join_a_webinar', 'local_intelliboard'); ?></span>
-                </a>
-            </div>
-        <?php else: ?>
-            <div class="intelliboard-support-text"><p><?php echo get_string('support_text4', 'local_intelliboard'); ?></p></div>
-            <div class="intelliboard-support-links actions">
-                <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.acuityscheduling.com/schedule.php">
-                    <span><?php echo get_string('support_demo', 'local_intelliboard'); ?></span>
-                </a>
-                <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.net/events">
-                    <span><?php echo get_string('join_a_webinar', 'local_intelliboard'); ?></span>
-                </a>
-                <a target="_blank" class="intelliboard-support-large-btn" href="https://support.intelliboard.net/hc/en-us/categories/360000794431-IntelliBoard-for-Moodle- ">
-                    <span><?php echo get_string('review_support_doc', 'local_intelliboard'); ?></span>
-                </a>
-            </div>
+        <?php elseif (!empty($intelliboard->token) and false): ?>
+            <a href="<?php echo $CFG->wwwroot ?>/local/intelliboard/<?php echo $event ?>/index.php?action=<?php echo ($event)?'':'dashboard' ?>">
+                <?php echo get_string('dashboard', 'local_intelliboard'); ?>
+            </a>
         <?php endif; ?>
-        <div class="intelliboard-support-contacts">
-            <strong><a href="https://intelliboard.net/">IntelliBoard.net</a></strong>
-            <strong><a href="mailto:Info@IntelliBoard.net">Info@IntelliBoard.net</a></strong>
-            <br>
-            <br>
-        </div>
-        <div class="intelliboard-support-play"></div>
-        <div class="intelliboard-support-player">
-            <div style="padding:56.25% 0 0 0;position:relative;">
-                <iframe id="intelliboardvideo" src="https://player.vimeo.com/video/390827913?title=0&byline=0&portrait=0"
-                        style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
-
-            <button type="button" class="btn btn-default">
-                <?php echo get_string('support_close', 'local_intelliboard'); ?>
-            </button>
-        </div>
-
-        <div class="intelliboard-support-terms">
-            © 2014 - <?php echo date("Y") ?> IntelliBoard, Inc.<br>
-            <?php echo get_string('support_terms', 'local_intelliboard'); ?>
-        </div>
-        <div class="intelliboard-support-bg"></div>
+    </div>
+    <div class="intelliboard-support-logo">
+        <a target="_blank" href="https://intelliboard.net/">
+            <img src="<?php echo $CFG->wwwroot ?>/local/intelliboard/assets/img/logo@3x.png">
+        </a>
     </div>
 
-    <script type="text/javascript">
-        jQuery(document).ready(function(){
-            var iframe = document.getElementById('intelliboardvideo');
-        var player = new Vimeo.Player(iframe);
+    <div class="clear"></div>
 
-            jQuery('.intelliboard-support-player button').click(function(){
-                jQuery('.intelliboard-support-player').hide();
-                player.pause();
-            });
-            jQuery('.intelliboard-support-play').click(function(){
-                jQuery('.intelliboard-support-player').show();
-                player.play();
-            });
+    <?php if (empty($intelliboard->token)): ?>
+        <div class="intelliboard-support-text">
+            <p>
+                <?php echo get_string('support_text3', 'local_intelliboard'); ?><br><br>
+                <?php echo get_string('support_connect', 'local_intelliboard', ["connect_link" => $connectlink->out()]); ?>
+            </p>
+        </div>
+
+        <div class="intelliboard-support-links actions">
+            <a class="intelliboard-support-large-btn" href="<?php echo $CFG->wwwroot ?>/local/intelliboard/<?php echo $event ?>/index.php?action=<?php echo ($event)?'':'dashboard' ?>">
+                <span><?php echo get_string('dashboard', 'local_intelliboard'); ?></span>
+            </a>
+          </div>
+
+
+        <div class="intelliboard-support-links initial-reports">
+            <?php foreach(intelli_initial_reports() as $report): ?>
+                <a class="intelliboard-support-btn" href="<?php echo $report["url"]; ?>">
+                    <?php echo $report["name"]; ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
+        <div class="intelliboard-support-links actions">
+            <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.acuityscheduling.com/schedule.php">
+                <span><?php echo get_string('support_demo', 'local_intelliboard'); ?></span>
+            </a>
+            <a class="intelliboard-support-large-btn" target="_blank"
+               href="https://support.intelliboard.net/hc/en-us/categories/360000794431-IntelliBoard-for-Moodle-">
+                <span><?php echo get_string('support_page', 'local_intelliboard'); ?></span>
+            </a>
+            <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.net/events">
+                <span><?php echo get_string('join_a_webinar', 'local_intelliboard'); ?></span>
+            </a>
+        </div>
+    <?php else: ?>
+        <div class="intelliboard-support-text"><p><?php echo get_string('support_text4', 'local_intelliboard'); ?></p></div>
+
+        <div class="intelliboard-support-links actions">
+            <a class="intelliboard-support-large-btn" href="<?php echo $CFG->wwwroot ?>/local/intelliboard/<?php echo $event ?>/index.php?action=<?php echo ($event)?'':'dashboard' ?>">
+                <span><?php echo get_string('dashboard', 'local_intelliboard'); ?></span>
+            </a>
+          </div>
+
+
+        <div class="intelliboard-support-links initial-reports">
+            <?php foreach(intelli_initial_reports() as $report): ?>
+                <a class="intelliboard-support-btn" href="<?php echo $report["url"]; ?>">
+                    <?php echo $report["name"]; ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
+        <div class="intelliboard-support-links actions">
+            <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.acuityscheduling.com/schedule.php">
+                <span><?php echo get_string('support_demo', 'local_intelliboard'); ?></span>
+            </a>
+            <a target="_blank" class="intelliboard-support-large-btn" href="https://intelliboard.net/events">
+                <span><?php echo get_string('join_a_webinar', 'local_intelliboard'); ?></span>
+            </a>
+            <a target="_blank" class="intelliboard-support-large-btn" href="https://support.intelliboard.net/hc/en-us/categories/360000794431-IntelliBoard-for-Moodle- ">
+                <span><?php echo get_string('review_support_doc', 'local_intelliboard'); ?></span>
+            </a>
+        </div>
+    <?php endif; ?>
+    <div class="intelliboard-support-contacts">
+        <strong><a href="https://intelliboard.net/">IntelliBoard.net</a></strong>
+        <strong><a href="mailto:Info@IntelliBoard.net">Info@IntelliBoard.net</a></strong>
+        <br>
+        <br>
+    </div>
+    <div class="intelliboard-support-play"></div>
+    <div class="intelliboard-support-player">
+        <div style="padding:56.25% 0 0 0;position:relative;">
+            <iframe id="intelliboardvideo" src="https://player.vimeo.com/video/390827913?title=0&byline=0&portrait=0"
+                    style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+
+        <button type="button" class="btn btn-default">
+            <?php echo get_string('support_close', 'local_intelliboard'); ?>
+        </button>
+    </div>
+
+
+    <div class="intelliboard-support-bg"></div>
+</div>
+<div class="intelliboard-support-terms">
+    © 2014 - <?php echo date("Y") ?> IntelliBoard, Inc.<br>
+    <?php echo get_string('support_terms', 'local_intelliboard'); ?>
+</div>
+
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        var iframe = document.getElementById('intelliboardvideo');
+    var player = new Vimeo.Player(iframe);
+
+        jQuery('.intelliboard-support-player button').click(function(){
+            jQuery('.intelliboard-support-player').hide();
+            player.pause();
         });
-    </script>
-<?php endif; ?>
+        jQuery('.intelliboard-support-play').click(function(){
+            jQuery('.intelliboard-support-player').show();
+            player.play();
+        });
+    });
+</script>
+
 <?php echo $OUTPUT->footer();
--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  * @website    https://intelliboard.net/
  */
 
-$plugin->version = 2020030613;
+$plugin->version = 2020031113;
 $plugin->requires = 2011120500;
 $plugin->release = '6.0.0';
 $plugin->maturity = MATURITY_STABLE;
