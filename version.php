9fae0c7c0b5fbda3e9744110531b9321c3b73648
Taras Leshko <taras@intelliboard.net>
2021-11-02 10:18:14 -0400

    Bug fixes and performance improvements

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  * @website    https://intelliboard.net/
  */
 
-$plugin->version = 2021101804;
+$plugin->version = 2021110104;
 $plugin->requires = 2011120500;
 $plugin->release = '7.0.0';
 $plugin->maturity = MATURITY_STABLE;
