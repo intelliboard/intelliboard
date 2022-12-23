2273c4c42604b7443970e8442ccc4c7837176cca
Taras Leshko <taras@intelliboard.net>
2022-12-23 16:45:51 -0500

    Fixes
    Fix #53

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  * @website    https://intelliboard.net/
  */
 
-$plugin->version = 2022112804;
+$plugin->version = 2022122204;
 $plugin->requires = 2011120500;
 $plugin->release = '7.0.0';
 $plugin->maturity = MATURITY_STABLE;
