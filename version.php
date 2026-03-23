aedb8737fd87c1ba37b8c28b99f2b74c3ea9e689
SDIntelliboard <stepand@intelliboard.net>
2026-03-23 15:01:56 +0200

    Bug fixes and performance improvements

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  * @website    https://intelliboard.net/
  */
 
-$plugin->version = 2026032301;
+$plugin->version = 2026032302;
 $plugin->requires = 2011120500;
-$plugin->release = '7.0.0';
+$plugin->release = '7.0.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_intelliboard';
